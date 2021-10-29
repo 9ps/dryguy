a944c34fe8ffa0db57c5c64c141b92b0cae64f35
9ps@pm.me <praveen>
2021-10-29 13:18:34 +1100

    mostly fully functional tracking

--- testE_dates/script.js
+++ testE_dates/script.js
@@ -1,4 +1,4 @@
-//these prototype methods are to set and get objects to Local storage
+//METHODS, use localStorage.<>()
 Storage.prototype.setObject = function(key, value) {
     this.setItem(key, JSON.stringify(value));
 }
@@ -7,35 +7,41 @@ Storage.prototype.getObject = function(key) {
     return value && JSON.parse(value);
 }
 
-// SELECTORS
-
-
 //IMPORTANT VARIABLES
+localStorage.setItem("limit", 2);
+localStorage.setObject("dryDays", [2, 4]);
+localStorage.setObject("Fri Oct 29 2021", { 'drinks': 100, 'triggers': [1, 0, 1, 0, 0, 0, 0, 0, 0, 0], 'done': 0, 'emotion': 0, 'reflections': ["", "", ""] });
+
+var dryDays = localStorage.getObject("dryDays");
+var limit = localStorage.getItem("limit");
 
 //DATE STUFF
-const daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+const daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 var d = new Date(); //right now
-var selectedDate = new Date(d.getFullYear(), d.getMonth(), d.getDate()); //stripped of time data
-
-var dates = [];
+var selectedDate = new Date(d.getFullYear(), d.getMonth(), d.getDate()); //current day (stripped of time data);
+var dates = []; //stores Date objects
 
 for(i = 0; i < 7; i++){ //big loop creating the [dates] and formatting the calender
-    if(dates.length == 0) {
+    if(dates.length == 0) { //sets today
         dates = [selectedDate];
-    } else {
+    } else { //sets other days
         dates[i] = new Date();
         dates[i].setDate(dates[i-1].getDate() - 1);
     }
 
-    var dateThing = document.createElement('div');
+    var currentDay = dates[i].getDay();
+    var currentDate = dates[i].getDate();
+
+    var dateThing = document.createElement('div'); //creating the date display frame
     dateThing.className = 'date';
     document.querySelector('.datesScroll').appendChild(dateThing);
     
-    var isCactus; //simple thing that adds cactus to Tue and Thu
-    if(dates[i].getDay() == 2 || dates[i].getDay() == 4) {
-        isCactus = "🌵";
-    } else {
-        isCactus = "";
+    var isCactus = ""; //simple thing that adds cactus to Tue and Thu
+
+    for(j = 0; j < dryDays.length; j++){
+        if(currentDay == dryDays[j]) {
+            isCactus = "🌵";
+        }
     }
 
     var dayCactus = document.createElement('div');
@@ -44,26 +50,31 @@ for(i = 0; i < 7; i++){ //big loop creating the [dates] and formatting the calen
     
     var dayText = document.createElement('div');
     dayText.className = 'dayText';
-    dayText.textContent = daysOfTheWeek[dates[i].getDay()];
+    dayText.textContent = daysOfTheWeek[currentDay];
     
-    var dateText = document.createElement('button');
+    var dateText = document.createElement('div');
     dateText.className = 'dateText';
     dateText.id = i; //dunno if this should be the date string, i think not
-    dateText.textContent = dates[i].getDate();
+    dateText.textContent = currentDate;
+
+    if(localStorage.getObject(dates[i].toDateString()) == null){ //creates a localStorage thing if it isnt found [i guess for reflection we check if null or reflections]
+        localStorage.setObject(dates[i].toDateString(), { 'drinks': 0, 'triggers': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'done': 0, 'emotion': 0, 'reflections': ["", "", ""] });
+    }
+
+    var e = localStorage.getObject(dates[i].toDateString()).drinks > limit; //dictating the color
+    if(e){
+        dateText.classList.add('dateTextOver');
+    } else {
+        dateText.classList.add('dateTextUnder');
+    }
 
     dateThing.appendChild(dayCactus);
     dateThing.appendChild(dayText);
     dateThing.appendChild(dateText);
 }    
 
-//TEMP THING
-//need to loop through this at the start to fill dates if they dont already exist...
-//or, if i click it, i could create it if it doesnt exist??
-localStorage.setObject(dates[0].toJSON(), { 'drinks': 100, 'triggers': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'done': 0, 'emotion': 0, 'reflections': ["", "", ""] });
-
 
 //DATE SWITCHER
-var dayData;
 const fullDate = document.querySelector('.fullDate');
 const drinksLoggedDisplay = document.querySelector('.drinksLoggedDisplay');
 var dateTexts = document.querySelectorAll('.dateText');
@@ -72,19 +83,18 @@ console.log("found", dateTexts.length, "dates");
 
 for (var i = 0; i < dateTexts.length; i++) {
     dateTexts[i].addEventListener('click', function() {
-    console.log("You clicked:", this.id);
-    this.classList.toggle("dateTextActive"); //TODO
     selectedDate = dates[this.id]; //this is important
+    console.log("Selected Calender Date: ", selectedDate.toDateString());
+    
+    dayData = localStorage.getObject(selectedDate.toDateString());
+
     fullDate.textContent = selectedDate.toDateString();
-
-    console.log("looking for: ", selectedDate.toJSON());
-    dayData = localStorage.getObject(selectedDate.toJSON());
-
     drinksLoggedDisplay.textContent = dayData.drinks + " Drinks Logged";
-    console.log("selected date: ", selectedDate);    
     });
 }
 
+//This is the current dayData!!
+var dayData = localStorage.getObject(dates[0].toDateString()); //this sets as current day!!
 
 //LOG MODAL
 var logModal = document.querySelector(".logModal");
@@ -94,6 +104,16 @@ var save = document.querySelector(".save");
 
 logButton.onclick = function() {
     logModal.style.display = "block";
+    drinkDisplay.textContent = dayData.drinks; //sets the content [?do we need exception handling]
+
+    for (var i = 0; i < triggerOptions.length; i++) { //sets the triggers
+        if(dayData.triggers[i] == 1){
+            triggerOptions[i].classList.add("triggerActive");
+        } else {
+            triggerOptions[i].classList.remove("triggerActive");
+        }
+    }
+
 }
 
 closeButton.onclick = function() {
@@ -101,45 +121,45 @@ closeButton.onclick = function() {
 }
 
 window.onclick = function(event) {
-  if (event.target == logModal) {
-    logModal.style.display = "none";
-  }
+    if (event.target == logModal) {
+        logModal.style.display = "none";
+    }
 }
 
 save.onclick = function() { //TODO
-    //we gotta package everything here
+    console.log("saving: ", selectedDate.toDateString());
+    localStorage.setObject(selectedDate.toDateString(), dayData);
     logModal.style.display = "none";
 }
 
 //DRINK COUNTING
-
 const drinkDisplay = document.querySelector('.drinkDisplay');
-var drinkTotal = 0;
+
 function drinkSub(){
-    if(drinkTotal <= 0){
+    if(dayData.drinks <= 0){
         return;
     }
-    drinkTotal -= 1;
-    drinkDisplay.textContent = drinkTotal;
+    dayData.drinks -= 1;
+    drinkDisplay.textContent = dayData.drinks;
 }
 
 function drinkAdd(){
-    drinkTotal += 1;
-    drinkDisplay.textContent = drinkTotal;
+    dayData.drinks += 1;
+    drinkDisplay.textContent = dayData.drinks;
 }
 
 //TRIGGERS
 
-var triggers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
-var triggerOptions = document.querySelectorAll('.trigger'); 
-console.log("found", triggerOptions.length, "triggers");
+var triggers = dayData.triggers;
+var triggerOptions = document.querySelectorAll('.trigger');
+
 for (var i = 0; i < triggerOptions.length; i++) {
     triggerOptions[i].addEventListener('click', function() {
 
     console.log("You toggled:", this.id);
     this.classList.toggle("triggerActive");
     
-    triggers[this.id] = 1 - triggers[this.id];
-    console.log(triggers);
+    dayData.triggers[this.id] = 1 - dayData.triggers[this.id];
+    console.log(dayData.triggers);
     });
 }

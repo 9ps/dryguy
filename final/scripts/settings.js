ca1e8f4f9dba56604d3a76dfe84782d48299926b
AnnieTheAvatar <annielacoba@yahoo.com>
2021-11-09 20:12:25 +1100

    settings bones

---
+++ final/scripts/settings.js
@@ -0,0 +1,17 @@
+function activate(info) {
+    let selected = document.getElementById(info);
+    selected.classList.toggle("hidden");
+};
+
+function select(id) {
+    let day = document.getElementById(id);
+    day.classList.toggle("selected");
+};
+
+function changeLimit() {
+
+};
+
+function reminder() {
+
+};

10abfec591f6c0545c8287c8501f18baf72c4442
AnnieTheAvatar <annielacoba@yahoo.com>
2021-11-11 14:19:08 +1100

    progress reasons relates to onboarding

--- final/scripts/progress.js
+++ final/scripts/progress.js
@@ -1,3 +1,59 @@
+//METHODS, use localStorage.<>()
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+
+let data = localStorage.getObject("data");
+if (data == null) { //if onboarding not complete
+    console.log("no Data, fallback");
+    localStorage.setObject("data", { name: "Ricky", 'dryDays': [0, 0, 1, 0, 1, 0, 0], 'dailyLimit': 2, 'majorGoal': 3, 'minorGoals': [1, 1, 1, 1, 0, 0, 0, 0] });
+    let data = localStorage.getObject("data");
+}
+
+const triggersList = ["Family", "Friends", "Work", "Occasions", "Routine", "Media", "Lonliness", "Stress", "Boredom", "Pain Relief"]
+const daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const majorList = ["Track Drinking", "Control Drinking", "Reduce Drinking", "Quit Drinking", "Maintain Sobriety"];
+const minorList = ["Better my Health", "Lose Weight", "Sleep Better", "Gain Control", "Cope Better", "Have Sober Time", "Be more Social", "Save Money", "Improve Relationships"];
+
+let mainGoal = document.getElementById('mainGoal');
+mainGoal.textContent = majorList[data.majorGoal]
+
+let count = 0;
+
+let mainReasons = document.getElementById('mainReasons');
+const countOccurrences = (arr) => arr.reduce((a, v) => (v === 1 ? a + 1 : a), 0);
+if (countOccurrences(data.minorGoals) > 1) {
+    console.log(data.minorGoals);
+    data.minorGoals.forEach(reason => {
+        //console.log(reason);
+        if (reason == 1) {
+            let reasonDisplay = document.createElement('button');
+            reasonDisplay.setAttribute('class', 'triggerActive');
+
+            reasonDisplay.textContent = minorList[count];
+            mainReasons.appendChild(reasonDisplay);
+        }
+        count += 1;
+        //console.log(count)
+    });
+} else if (countOccurrences(data.minorGoals) == 0) {
+    let reasonDisplay = document.createElement('button');
+    reasonDisplay.setAttribute('class', 'triggerActive');
+
+    reasonDisplay.textContent = minorList[count];
+    mainReasons.appendChild(reasonDisplay);
+}
+
+
+var dailyLimit = data.dailyLimit;
+
+const dailyGoal = document.querySelector("#dailyGoal");
+dailyGoal.textContent = "Daily Limit of " + dailyLimit + " Drinks";
+
 const overviewContent = document.getElementById('overview');
 const reflectionsContent = document.getElementById('reflections');
 const reasonsContent = document.getElementById('reasons');
@@ -64,3 +120,7 @@ function closeSupport() {
     popup.classList.add('hidden');
     overviewContent.classList.remove('hidden');
 }
+
+Object.keys(localStorage).forEach(function(key) {
+    //console.log(localStorage.getItem(key));
+});

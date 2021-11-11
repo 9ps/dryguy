f2195b20d2d826d724dd2352046b7b1fbc60225f
AnnieTheAvatar <annielacoba@yahoo.com>
2021-11-12 10:58:05 +1100

    lil changes

---
+++ scripts/profile.js
@@ -0,0 +1,15 @@
+//METHODS, use localStorage.<>()
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+
+let data = localStorage.getObject("data");
+if (data == null) { //if onboarding not complete
+    console.log("no Data, fallback");
+    localStorage.setObject("data", { name: "Ricky", 'dryDays': [0, 0, 1, 0, 1, 0, 0], 'dailyLimit': 2, 'majorGoal': 3, 'minorGoals': [1, 1, 1, 1, 0, 0, 0, 0] });
+    let data = localStorage.getObject("data");
+}

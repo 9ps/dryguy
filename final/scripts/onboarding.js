bc33cee240b9d2ddddfa080d8ee7dcc2a1603613
9ps@pm.me <9ps@pm.me>
2021-11-12 00:55:43 +1100

    onboarding: validation

--- final/scripts/onboarding.js
+++ final/scripts/onboarding.js
@@ -13,9 +13,10 @@ const next = document.querySelector("#next");
 const onboardingProgress = document.querySelector("#onboardingProgress");
 const pages = document.querySelectorAll(".page");
 const begin = document.querySelector("#begin");
-const nameInput = document.querySelector('#nameInput'); //all the emotions
+const nameInput = document.querySelector('#nameInput');
 const majorGoalDisplay = document.querySelector('#majorGoalDisplay');
 
+var valid = true;
 var data = {'name': "", 'dryDays': [0, 0, 0, 0, 0, 0, 0], 'dailyLimit': 0, 'majorGoal': 0, 'minorGoals': [0, 0, 0, 0, 0, 0, 0, 0, 0]};
 var currentPage = 0;
 const majorList = ["Track Drinking", "Control Drinking", "Reduce Drinking", "Quit Drinking", "Maintain Sobriety"];
@@ -46,8 +47,19 @@ function showPage() {
   }
 }
 
-function changePage(n) {
-  
+function changePage(n) { //n = 1, -1
+
+  if(n == 1 && !valid) {
+    return;
+  } else if(n == 1 && valid) {
+    console.log("c");
+    valid = 0;
+    next.classList.add("buttonInactive");  
+  } else {
+    console.log("e");
+    updateButton();
+  }
+
   localStorage.setObject("data", data);
 
   if(currentPage == 1){
@@ -58,20 +70,34 @@ function changePage(n) {
     majorGoalDisplay.innerText = majorList[data.majorGoal];
   }
 
+  if(currentPage == 4){
+    updateButton();
+  }
+
   pages[currentPage].style.display = "none";
   currentPage = currentPage + n;
   showPage();
 }
 
 
-// Major Goal Selector
+nameInput.addEventListener('input', evt => { 
+  updateButton();
+});
 
-const majorGoals = document.querySelectorAll('.majorGoal'); //all the emotions
+function updateButton(){
+  if(!valid){
+    valid = true;
+    next.classList.remove("buttonInactive");
+  }
+};
+
+// Major Goal Selector
+const majorGoals = document.querySelectorAll('.majorGoal');
 
 for (var i = 0; i < majorGoals.length; i++) {
-	majorGoals[i].addEventListener('click', function() { //if an emotion is clicked
-		
-		for (var i = 0; i < majorGoals.length; i++) { //set every other emotion to inactive looking
+	majorGoals[i].addEventListener('click', function() {
+		updateButton();
+		for (var i = 0; i < majorGoals.length; i++) {
 			majorGoals[i].classList.remove("buttonActive");
 		}
 
@@ -86,6 +112,7 @@ const minorGoals = document.querySelectorAll('.minorGoal');
 
 for (var i = 0; i < minorGoals.length; i++) {
 	minorGoals[i].addEventListener('click', function() {
+    updateButton();
     this.classList.toggle("triggerActive");
     var minorGoalIndex = removeLetters(this.id, "minorGoal");
     data.minorGoals[minorGoalIndex] = 1 - data.minorGoals[minorGoalIndex];
@@ -99,7 +126,7 @@ function changeLimit(n){
   if(data.dailyLimit <= 0 && n == -1){
 		return;
 	}
-
+  updateButton();
 	data.dailyLimit += n;
 	limitDisplay.textContent = data.dailyLimit;
 }
@@ -125,36 +152,3 @@ function pushToStorage(){
   localStorage.setObject("data", data);
 }
 
-
-
-//Modal stuff
-
-/*
-const questionButton1 = document.querySelector("#questionButton1");
-const questionButton2 = document.querySelector("#questionButton2");
-const questionModal1 = document.querySelector("#questionModal1");
-const questionModal2 = document.querySelector("#questionModal2");
-const background1 = document.querySelector("#background1");
-const background2 = document.querySelector("#background2");
-
-questionButton1.onclick = function() {
-  console.log("clicked");
-  background1.style.display = "block";
-	questionModal1.style.display = "block";
-}
-
-questionButton2.onclick = function() {
-  background2.style.display = "block";
-	questionModal2.style.display = "block";
-}
-
-window.onclick = function(event) {
-	if (event.target == background1) {
-    background1.style.display = "none";
-		questionModal1.style.display = "none"
-  } else if (event.target == background2) {
-    background2.style.display = "none";
-		questionModal2.style.display = "none";
-  }
-}
-*/
